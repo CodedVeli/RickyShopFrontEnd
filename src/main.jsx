2a5219a9b015b384f56a0eb313ef7b2d39cafee5
eric githaiga <ericgithaiga007@gmail.com>
2024-04-03 22:29:54 +0300

    hashrouting implentation

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import { store } from "./components/rtk/store";
 import { Provider } from "react-redux";
 import {AuthProvider} from './utils/Auth'
-import { fetchProduct } from "./components/rtk/features/ProductSlice.jsx";
-import { productApi } from "./components/rtk/features/Apis/ProductApi.jsx";
 
-// store.dispatch(fetchProduct())
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Router>      
